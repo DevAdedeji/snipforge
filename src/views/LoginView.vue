c5e1c503934d45c011f3fdea331d3bc4f1e42d04
DevAdedeji <adedejitewogbade2@gmail.com>
2025-10-30 23:49:37 +0100

    feat: integrate @nuxt/ui and implement authentication features
    - Added @nuxt/ui dependency for UI components.
    - Wrapped RouterView in UApp component in App.vue.
    - Removed base.css and updated main.css to use Tailwind CSS and @nuxt/ui styles.
    - Set up Firebase authentication with Google and GitHub providers.
    - Created LoginView.vue for user login with social authentication.
    - Implemented route guards in router/index.ts to protect routes.
    - Added composables for authentication logic in src/composables/auth/index.ts.
    - Updated HomeView.vue to display a centered message.
    - Added SVG icons for Google and GitHub in the public directory.
    - Configured Firebase in src/firebase/index.ts.

---
+++ src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<template>
+    <main class="w-full h-screen flex items-center justify-center text-text-primary-dark">
+        <div class="bg-card-dark/50! p-4 sm:p-8 shadow-subtle backdrop-blur-sm border border-white/10 flex w-full max-w-[90%] mx-auto sm:max-w-md flex-col items-center justify-center gap-4 rounded-lg">
+            <div class="flex items-center gap-2 pb-4">
+                <img src="/logo.svg" alt="">
+            </div>
+            <h1 class="text-text-primary-dark tracking-tight text-3xl font-bold leading-tight text-center pb-2">Sign in to SnipForge</h1>
+            <p class="text-text-secondary-dark text-base font-normal leading-normal text-center pb-6">Access your snippets and boost your productivity.</p>
+            <div class="flex w-full flex-col items-stretch gap-4">
+                <UButton color="neutral" variant="outline" class="bg-[#2d3748]! text-text-primary-dark" @click="loginWithGoogle">
+                    <img src="/google.svg" class="size-5" alt="google logo">
+                    Continue with Google
+                </UButton>
+                <UButton color="neutral" variant="outline" class="bg-[#2d3748]! text-text-primary-dark" @click="loginWithGithub">
+                    <img src="/github.svg" class="size-5" alt="github logo">
+                    Continue with Github
+                </UButton>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script setup lang="ts">
+import { useAuth } from "@/composables/auth"
+const { loginWithGithub, loginWithGoogle } = useAuth()
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,24 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import ui from '@nuxt/ui/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    ui(
+      {
+        ui: {
+          button: {
+            slots: {
+              base: ['h-12 flex items-center justify-center']
+            }
+          }
+        }
+      }
+    )
   ],
   resolve: {
     alias: {
