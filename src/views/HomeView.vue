6c16817a60a625a9263b74d1b3039236dd603a46
DevAdedeji <adedejitewogbade2@gmail.com>
2025-10-31 17:22:38 +0100

    style: format HomeView.vue for improved readability

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,24 +6,34 @@
           <img src="/logo.svg" alt="" />
           <p class="text-xl font-bold leading-tight tracking-[-0.015em]">SnipForge</p>
         </div>
-        <router-link to="/login" class="bg-primary text-text-primary-dark rounded-lg h-10 px-4 flex items-center justify-center font-medium">
+        <router-link
+          to="/login"
+          class="bg-primary text-text-primary-dark rounded-lg h-10 px-4 flex items-center justify-center font-medium"
+        >
           <UIcon name="i-heroicons-plus" class="block sm:hidden text-2xl" />
           <span class="hidden sm:block">Create Snippets</span>
         </router-link>
       </nav>
     </header>
-    <div class="max-w-[90%] mx-auto flex flex-col gap-4 items-center justify-center h-[80vh] text-center">
-      <h1 class="text-text-primary-dark text-4xl font-black leading-tight tracking-[-0.033em] sm:text-5xl md:text-6xl">
-          Discover &amp; Share Code Snippets
+    <div
+      class="max-w-[90%] mx-auto flex flex-col gap-4 items-center justify-center h-[80vh] text-center"
+    >
+      <h1
+        class="text-text-primary-dark text-4xl font-black leading-tight tracking-[-0.033em] sm:text-5xl md:text-6xl"
+      >
+        Discover &amp; Share Code Snippets
       </h1>
-      <p class="text-text-secondary-dark text-base font-normal leading-normal  mx-auto sm:text-lg">
-          The ultimate vault for developers. Explore a world of elegant solutions and contribute your own.
+      <p class="text-text-secondary-dark text-base font-normal leading-normal mx-auto sm:text-lg">
+        The ultimate vault for developers. Explore a world of elegant solutions and contribute your
+        own.
       </p>
-      <UButton color="primary" class="mt-4 text-text-primary-dark font-medium text-base">Explore Snippets</UButton>
+      <UButton color="primary" class="mt-4 text-text-primary-dark font-medium text-base"
+        >Explore Snippets</UButton
+      >
     </div>
   </main>
 </template>
 
 <script setup lang="ts">
-import { RouterLink } from "vue-router";
+import { RouterLink } from 'vue-router'
 </script>
